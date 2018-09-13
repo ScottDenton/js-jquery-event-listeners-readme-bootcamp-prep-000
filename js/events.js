484ae8409c64da37a0004a5538667e11aab22573
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-13 21:13:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,22 @@
 //define functions here
 
+function getIt(){
+    $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').style.className = 'tasty';
+  })
+}
+
 $(document).ready(function(){
 
+
 // call functions here
+
+
 
 });
