62cbdf8cd8cff436fd6d20c0df1eb8caa8e2bb7c
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-13 21:22:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt(){
 }
 
 function pressIt() {
-  ${'typing'}.on('keydown', function(){
+  $('typing').on('keydown', function(){
     if(e.which === 71) {
       alert("The G key was pressed")
     }
@@ -28,6 +28,6 @@ $(document).ready(function(){
 
 
 });
-function pressIt(){
-  ${'typing'} 
+function pressIt(){ 
+ 
 }
